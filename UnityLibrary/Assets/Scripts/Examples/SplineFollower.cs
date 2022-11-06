a8017407a6ad464b9998fd839b64e12aa3ea66f2
alimertcetin <mrtmert@gmail.com>
2022-11-07 00:23:21 +0300

    SplineFollower.cs

---
+++ UnityLibrary/Assets/Scripts/Examples/SplineFollower.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using XIV.Math;
+using XIV.Spline;
+
+namespace Examples
+{
+	public class SplineFollower : MonoBehaviour
+	{
+		public BezierSpline spline;
+
+		public float moveSpeed;
+		float timePassed;
+		float totalMovementDuration;
+		
+		void Update()
+		{
+			totalMovementDuration = spline.Length / moveSpeed;
+			timePassed = (timePassed + Time.deltaTime) % totalMovementDuration;
+			var normalizedTime = timePassed / totalMovementDuration;
+
+			var pos = spline.GetPoint(normalizedTime);
+			transform.position = pos;
+		}
+	}
+}
